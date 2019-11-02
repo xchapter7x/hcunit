d7273c16ddce53bcb4f09645e2265c5dc8104f76
John Calabrese <xchapter7x@gmail.com>
2019-11-02 08:11:27 -0400

    safe defaults for empty version incrementing

--- bin/create_new_release.sh
+++ bin/create_new_release.sh
@@ -6,7 +6,13 @@ git pull --tags >/dev/null || true
 
 echo "generate a rc build number"
 BUMP_SEMVER_PATCH=$(git tag -l | grep -v "-" | tail -1 | awk -F. '{print $1"."$2"."$3+1}')
+if [[ ${BUMP_SEMVER_PATCH} -eq ".." ]]; then
+  BUMP_SEMVER_PATCH="0.1.0"
+fi
 BUMP_SEMVER_RC=$(git tag -l | grep "${BUMP_SEMVER_PATCH}" | grep -e "-rc" | tail -1 | awk -F"-rc." '{print $2+1}')
+if [[ ${BUMP_SEMVER_RC} -eq "-rc." ]]; then
+  BUMP_SEMVER_RC="-rc.0"
+fi
 SEMVER=${BUMP_SEMVER_PATCH}-rc.${BUMP_SEMVER_RC}
 echo "tag id is: "${SEMVER}
 echo "creating plugin tarball"
